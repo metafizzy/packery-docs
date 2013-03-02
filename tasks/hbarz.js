f1cd3ed593ec12c55a84991a03174c6868621c38
David DeSandro <david@desandro.com>
2013-03-02 13:27:56 -0500

    add syntax highlighting

--- tasks/hbarz.js
+++ tasks/hbarz.js
@@ -1,5 +1,45 @@
 var path = require('path');
 var handlebars = require('handlebars');
+var highlightjs = require('highlight.js');
+
+// alias XML syntax highlighting as HTML
+highlightjs.LANGUAGES.html = highlightjs.LANGUAGES.xml;
+highlightjs.LANGUAGES.js = highlightjs.LANGUAGES.javascript;
+
+// parses content for ``` code blocks
+// use highlight.js tfor syntax highlighting
+// var reFenceBlock = /```((.|\n)+[^(```)])```/gi;
+var reFirstLine = /.*\n/;
+function highlight( content ) {
+  // split by ```, g
+  var splitContent = content.split(/\n```/);
+  splitContent.forEach( function( block, i ) {
+    // get every other block, which is the code
+    if ( i % 2 === 0 ) {
+      return;
+    }
+
+    var langMatch = block.match( reFirstLine );
+    var language = langMatch && langMatch[0];
+    // remove first line
+    if ( language ) {
+      block = block.replace( reFirstLine, '' );
+      language = language.trim();
+    }
+    //
+    var highlighted;
+    if ( language ) {
+      highlighted = highlightjs.highlight( language, block ).value;
+    } else {
+      highlighted = block;
+    }
+    // set content back with HTML
+    splitContent[i] = '<pre><code' +
+      ( language ? ' class="' + language + '"' : '' ) + '>' +
+      highlighted + '</code></pre>';
+  });
+  return splitContent.join('\n');
+}
 
 module.exports = function( grunt ) {
 
@@ -30,6 +70,7 @@ module.exports = function( grunt ) {
         }
         var dest = file.dest + '/' + splitPath.join( path.sep );
         // process source by template
+        src = highlight( src );
         var templated = templates[ opts.defaultTemplate ]({
           content: src,
           site: site
