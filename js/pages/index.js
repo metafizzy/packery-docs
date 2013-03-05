259c080dfc04628a212a3b67b76e9b54b951b048
David DeSandro <david@desandro.com>
2013-03-04 21:49:15 -0500

    pretty animation heros

--- js/pages/index.js
+++ js/pages/index.js
@@ -6,6 +6,12 @@
 
 'use strict';
 
+var PS = window.PS;
+
+/**
+ * create and return an item element
+ * @returns {Element} item
+ */
 function getItem( isRando ) {
   var item = document.createElement('div');
   var wRand = Math.random();
@@ -16,10 +22,38 @@ function getItem( isRando ) {
   item.className = 'item ' + widthClass + ' ' + heightClass;
   // random sizing
   if ( isRando ) {
-    item.style.width = Math.random() * Math.random() * 60 + 16 + 'px';
-    item.style.height = Math.random() * Math.random() * 60 + 16 + 'px';
+    item.style.width =  Math.round( Math.random() * Math.random() * 110 + 25 ) + 'px';
+    item.style.height = Math.round( Math.random() * Math.random() * 110 + 25 ) + 'px';
   }
   return item;
+}
+
+/**
+ * add items to a Packery
+ * keep doing it until it's full
+ */
+function addItems( pckry, isRando ) {
+  // stop after packery reaches height
+  if ( pckry.maxY > 350 ) {
+    return;
+  }
+  var fragment = document.createDocumentFragment();
+  var items = [];
+  for ( var i=0; i < 1; i++ ) {
+    var item = getItem( isRando );
+    items.push( item );
+    fragment.appendChild( item );
+    var draggie = new Draggabilly( item );
+    pckry.bindDraggabillyEvents( draggie );
+  }
+
+  pckry.element.appendChild( fragment );
+  pckry.appended( items );
+  // do it again
+  setTimeout( function() {
+    addItems( pckry, isRando );
+  }, 40 );
+
 }
 
 PS.index = function() {
@@ -31,26 +65,14 @@ PS.index = function() {
     gutter: 4
   });
 
-  function addGridItems() {
-    if ( gridPckry.maxY > 350 ) {
-      return;
-    }
-    var fragment = document.createDocumentFragment();
-    var items = [];
-    for ( var i=0; i < 1; i++ ) {
-      var item = getItem();
-      items.push( item );
-      fragment.appendChild( item );
-      var draggie = new Draggabilly( item );
-      gridPckry.bindDraggabillyEvents( draggie );
-    }
+  addItems( gridPckry, false );
 
-    gridElem.appendChild( fragment );
-    gridPckry.appended( items );
-    setTimeout( addGridItems, 40 );
-  }
+  var randoElem = document.querySelector('#rando-packery');
+  var randoPckry = new Packery( randoElem, {
+    gutter: 4
+  });
 
-  addGridItems();
+  addItems( randoPckry, true );
 
 };
 
