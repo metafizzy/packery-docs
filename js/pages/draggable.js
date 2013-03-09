66107d358df2f0a88b424196919c1e9d4ef79329
David DeSandro <david@desandro.com>
2013-03-09 09:39:43 -0500

    draggable grid docs

--- js/pages/draggable.js
+++ js/pages/draggable.js
@@ -46,6 +46,41 @@ PS.draggable = function() {
     pckry.bindUIDraggableEvents( $itemElems );
   })();
 
+  // ----- non-grid ----- //
+
+  ( function() {
+    var container = document.querySelector('#non-grid-demo .packery');
+    var itemElems = container.querySelectorAll('.item');
+    var pckry = new Packery( container );
+    // for each item element
+    for ( var i=0, len = itemElems.length; i < len; i++ ) {
+      var elem = itemElems[i];
+      // make element draggable with Draggabilly
+      var draggie = new Draggabilly( elem );
+      // bind Draggabilly events to Packery
+      pckry.bindDraggabillyEvents( draggie );
+    }
+  })();
+
+  // ----- grid demo ----- //
+
+  ( function() {
+    var container = document.querySelector('#grid-demo .packery');
+    var itemElems = container.querySelectorAll('.item');
+    var pckry = new Packery( container, {
+      columnWidth: 80,
+      rowHeight: 80
+    });
+    // for each item element
+    for ( var i=0, len = itemElems.length; i < len; i++ ) {
+      var elem = itemElems[i];
+      // make element draggable with Draggabilly
+      var draggie = new Draggabilly( elem );
+      // bind Draggabilly events to Packery
+      pckry.bindDraggabillyEvents( draggie );
+    }
+  })();
+
 };
 
 })( window );
