7eb0291b557f5b77185884006995bf8e770b2c5e
David DeSandro <david@desandro.com>
2013-03-09 15:07:24 -0500

    move highlight to separate file

--- tasks/hbarz.js
+++ tasks/hbarz.js
@@ -1,10 +1,7 @@
 var path = require('path');
 var handlebars = require('handlebars');
-var highlightjs = require('highlight.js');
 
-// alias XML syntax highlighting as HTML
-highlightjs.LANGUAGES.html = highlightjs.LANGUAGES.xml;
-highlightjs.LANGUAGES.js = highlightjs.LANGUAGES.javascript;
+var highlight = require('./utils/highlight');
 
 // -------------------------- Handlebar Helpers -------------------------- //
 
@@ -14,43 +11,6 @@ handlebars.registerHelper( 'if_equal', function( a, b, options ) {
     return options.fn( this );
   }
 });
-
-// --------------------------  -------------------------- //
-
-// parses content for ``` code blocks
-// use highlight.js tfor syntax highlighting
-// var reFenceBlock = /```((.|\n)+[^(```)])```/gi;
-var reFirstLine = /.*\n/;
-function highlight( content ) {
-  // split by ```, g
-  var splitContent = content.split(/\n```/);
-  splitContent.forEach( function( block, i ) {
-    // get every other block, which is the code
-    if ( i % 2 === 0 ) {
-      return;
-    }
-
-    var langMatch = block.match( reFirstLine );
-    var language = langMatch && langMatch[0];
-    // remove first line
-    if ( language ) {
-      block = block.replace( reFirstLine, '' );
-      language = language.trim();
-    }
-    //
-    var highlighted;
-    if ( language ) {
-      highlighted = highlightjs.highlight( language, block ).value;
-    } else {
-      highlighted = block;
-    }
-    // set content back with HTML
-    splitContent[i] = '<pre><code' +
-      ( language ? ' class="' + language + '"' : '' ) + '>' +
-      highlighted + '</code></pre>';
-  });
-  return splitContent.join('\n');
-}
 
 // -------------------------- parseJSONFrontMatter -------------------------- //
 
