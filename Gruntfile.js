a41c9d00db7c1a1c1f9b7ec1bb9d11cfda8ba543
David DeSandro <david@desandro.com>
2013-03-10 15:53:31 -0400

    move bower tasks into separate files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,4 @@
 
-var path = require('path');
-var spawn = require('child_process').spawn;
-
-// -------------------------- bower list helpers -------------------------- //
-
-function getDependencySrcs( list ) {
-  var srcs = [];
-  var dependency, main;
-  for ( var name in list ) {
-    dependency = list[ name ];
-    main = dependency.source && dependency.source.main;
-
-    if ( dependency.dependencies ) {
-      var depSrcs = getDependencySrcs( dependency.dependencies );
-      srcs.push.apply( srcs, depSrcs );
-    }
-
-    // add main sources to srcs
-    if ( main ) {
-      if ( Array.isArray( main ) ) {
-        srcs.push.apply( srcs, main );
-      } else {
-        srcs.push( main );
-      }
-    }
-
-  }
-  return srcs;
-}
-
-function organizeSources( tree ) {
-  // flat source filepaths
-  var srcs = getDependencySrcs( tree );
-  // remove duplicates, organize by file extension
-  var sources = {};
-
-  srcs.forEach( function ( src ) {
-    var ext = path.extname( src );
-    sources[ ext ] = sources[ ext ] || [];
-    if ( sources[ ext ].indexOf( src ) === -1 ) {
-      sources[ ext ].push( src );
-    }
-  });
-
-  return sources;
-}
-
-
 // -------------------------- grunt -------------------------- //
 
 module.exports = function( grunt ) {
@@ -65,7 +17,7 @@ module.exports = function( grunt ) {
         dest: 'build/js/packery-site.js'
       },
       pkgd: {
-        // src will be set in bower-map task
+        // src will be set in package-sources task
         dest: 'build/packery.pkgd.js'
       },
       css: {
@@ -82,7 +34,7 @@ module.exports = function( grunt ) {
       },
       js: {
         files: {
-          // 'build/js/packery-site.min.js' will be set
+          // 'build/js/packery-site.min.js' will be set in bower-list-map
         }
       }
     },
@@ -110,78 +62,12 @@ module.exports = function( grunt ) {
   });
 
 
-  // re-used vars
-  var bowerMap, packerySources;
-
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
+  // load all tasks in tasks/
   grunt.loadTasks('tasks/');
 
-  grunt.registerTask( 'default', 'bower-map packery-sources concat uglify hbarz'.split(' ') );
-  // grunt.registerTask( 'default', function() {
-  // });
-
-  grunt.registerTask( 'bower-fun', 'bower-map packery-sources'.split(' ') );
-
-
-  grunt.registerTask( 'bower-map', function() {
-    var done = this.async();
-    var childProc = spawn('bower', 'list --map'.split(' ') );
-
-    var mapSrc = '';
-
-    childProc.stdout.setEncoding('utf8');
-    childProc.stdout.on('data',  function( data ) {
-      mapSrc += data;
-    });
-
-    childProc.on('close', function() {
-      bowerMap = JSON.parse( mapSrc );
-      // delete bowerMap.jquery;
-      var bowerSources = organizeSources( bowerMap );
-      // remove jQuery, EventEmitter.min.js
-      var bowerJsSources = bowerSources['.js'].filter( function( src ) {
-        return src.indexOf('/jquery.js') === -1 &&
-          src.indexOf('.min.js') === -1;
-      });
-      // add bower JS to JS collection
-      var jsSrcs = grunt.config.get('concat.js.src');
-      jsSrcs = bowerJsSources.concat( jsSrcs );
-      grunt.config.set( 'concat.js.src', jsSrcs );
-      grunt.config.set( 'uglify.js.files', {
-        'build/js/packery-site.min.js': jsSrcs
-      });
-
-      // add CSS sources from Bower
-      if ( bowerSources['.css'] && bowerSources['.css'].length ) {
-        var cssSrcs = grunt.config.get( 'concat.css.src' );
-        cssSrcs.push.apply( cssSrcs, bowerSources['.css'] );
-        // console.log( sources['.css'], cssSrcs );
-        grunt.config.set( 'concat.css.src', cssSrcs );
-      }
-
-      done();
-    });
-
-  });
-
-  // create packery.pkgd.js
-  grunt.registerTask( 'packery-sources', function() {
-    // copy over just the packery obj
-    var packeryMap = {
-      packery: bowerMap.packery
-    };
-
-    packerySources = organizeSources( packeryMap );
-    // console.log( packerySources );
-    var srcs = packerySources['.js'];
-    // filter out minified files, like EventEmitter.min.js
-    srcs = srcs.filter( function( src ) {
-      return src.indexOf('.min.js') === -1;
-    });
-    grunt.config.set( 'concat.pkgd.src', srcs );
-  });
+  grunt.registerTask( 'default', 'bower-list-map package-sources concat uglify hbarz'.split(' ') );
 
 };
--- notes.md
+++ notes.md
@@ -1,10 +1,3 @@
 copy all bower components over to build
 
-add `.row` to `.example`
-
 add jsFiddles example links
-
-move bower tasks to separate file
-
-## Appendix
-
