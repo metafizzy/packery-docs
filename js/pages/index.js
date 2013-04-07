febbf3069e42f7df498b06fd0a9a3532e279e12b
David DeSandro <david@desandro.com>
2013-04-07 19:40:59 -0400

    change page nav color when scrolling

--- js/pages/index.js
+++ js/pages/index.js
@@ -10,6 +10,21 @@ var PS = window.PS;
 
 var Draggabilly = window.Draggabilly;
 
+// --------------------------  -------------------------- //
+
+var defView = document.defaultView;
+
+var getStyle = defView && defView.getComputedStyle ?
+  function( elem ) {
+    return defView.getComputedStyle( elem, null );
+  } :
+  function( elem ) {
+    return elem.currentStyle;
+  };
+
+
+// --------------------------  -------------------------- //
+
 /**
  * create and return an item element
  * @returns {Element} item
@@ -157,6 +172,44 @@ PS.index = function() {
       gutter: 4
     });
     bindDraggies( pckry );
+  })();
+
+  // ----- scroll stuff ----- //
+
+  ( function() {
+    var pageNav = document.querySelector('#page-nav');
+    var isAtTop = true;
+    // add initial class
+    var style = getStyle( pageNav );
+    if ( style.position === 'absolute' || style.position === 'fixed' ) {
+      classie.add( pageNav, 'is-at-top' );
+    }
+    // only add scroll event if fixed
+    if ( style.position !== 'fixed' ) {
+      return;
+    }
+
+    var navY = getSize( pageNav ).height / 2 + parseInt( style.top, 10 );
+    var installHeader = document.querySelector('#install');
+    var contentY = installHeader.getBoundingClientRect().top;
+
+    var scrollTimeout;
+
+    // debounce scroll
+    eventie.bind( window, 'scroll', function() {
+      if ( scrollTimeout ) {
+        clearTimeout( scrollTimeout );
+      }
+      scrollTimeout = setTimeout( onDebounceScroll, 100 );
+    });
+
+    function onDebounceScroll() {
+      var wasAtTop = isAtTop;
+      isAtTop = window.scrollY + navY < contentY;
+      if ( isAtTop !== wasAtTop ) {
+        classie[ isAtTop ? 'add' : 'remove' ]( pageNav, 'is-at-top' );
+      }
+    }
   })();
 
 };
